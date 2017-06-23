d267de8e4f7aeeaf7f4d99ec764ab2ac669baffa
kshavonina <kshavonina@yandex.ru>
2017-06-23 19:54:11 +0300

    added equals() and hashcode() for Cat class

--- src/main/java/June22/Cat.java
+++ src/main/java/June22/Cat.java
@@ -9,6 +9,22 @@ public class Cat {
 
     public void setName(String name) {
         this.name = name;
+
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cat cat = (Cat) o;
+
+        return name != null ? name.equals(cat.name) : cat.name == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return name != null ? name.hashCode() : 0;
     }
 
     //jnknjl
