d407f001e581c2c810c1070cc4b3319629511f56
kshavonina <kshavonina@yandex.ru>
2017-06-26 21:28:13 +0300

    kkkkkkkk

--- src/test/java/June26/MainTest.java
+++ src/test/java/June26/MainTest.java
@@ -31,6 +31,7 @@ public class MainTest {
     public void countDegree() {
         count--;
     }
+
 // kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
 // kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
 
