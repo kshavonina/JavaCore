43b2623b64a7fb55b42a187c2eb02a4b5b16c940
kshavonina <kshavonina@yandex.ru>
2017-07-06 20:06:03 +0300

    String and StringBuilder (examples)

--- src/main/java/July6/Main.java
+++ src/main/java/July6/Main.java
@@ -8,8 +8,16 @@ public class Main {
         // но это не значит, что будет изменена существуетвующая строка.
         // просто будет выделена еще память отдельно под строку " Daniels"
 
-        
+        StringBuffer buffer = new StringBuffer();
+        StringBuilder builder = new StringBuilder();
 
+        for (int i = 0; i < 10; i++) {
+            s = s + i;
+        }
+    }
 
+    private String getString() {
+        StringBuilder sb = new StringBuilder();
+        return sb.append("first").append("second").toString();
     }
 }
