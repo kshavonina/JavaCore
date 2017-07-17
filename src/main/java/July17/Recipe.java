e171b89e4c964a0b6e177158e7b1a3de92748389
kshavonina <kshavonina@yandex.ru>
2017-07-17 20:49:49 +0300

    Recipe class

---
+++ src/main/java/July17/Recipe.java
@@ -0,0 +1,39 @@
+package July17;
+
+import java.io.IOException;
+
+public class Recipe {
+    // кастрюля с кешем
+    public static boolean flag = true;
+
+    public static void main(String[] args) {
+        new Recipe.ThreadFlagSetter().start();
+        new Recipe.ThreadFlagReader().start();
+    }
+
+    public static class ThreadFlagSetter extends Thread {
+        @Override
+        public void run() {
+            try {
+                int k = System.in.read();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            flag = false; // кеш кастрюли
+            System.err.println("Flag now is false");
+        }
+    }
+
+    public static class ThreadFlagReader extends Thread {
+        @Override
+        public void run() {
+            System.err.println("waiting...");
+            while (flag) {
+
+            }
+
+            System.err.println("Go go");
+        }
+    }
+}
