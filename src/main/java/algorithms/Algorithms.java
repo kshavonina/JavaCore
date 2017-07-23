58e19ddb7bbb7712bd5c0ece314032d804adcbcf
kshavonina <kshavonina@yandex.ru>
2017-07-23 20:57:11 +0300

    add insertion sort

--- src/main/java/algorithms/Algorithms.java
+++ src/main/java/algorithms/Algorithms.java
@@ -1,8 +1,8 @@
 package algorithms;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import sun.java2d.windows.GDIRenderer;
+
+import java.util.*;
 
 public class Algorithms {
     public static void main(String[] args) {
@@ -11,7 +11,7 @@ public class Algorithms {
         System.out.println(listLength);
 
         for (int i = 0; i < listLength; i++) {
-            list.add((int) (Math.random() * 200));
+            list.add((int) (Math.random() * 99));
         }
 
         System.out.println(list);
@@ -19,10 +19,8 @@ public class Algorithms {
         Algorithms algs = new Algorithms();
         System.out.println("List sorted by Bubble Sort:");
         System.out.println(algs.bubbleSort(list));
-
-        System.out.println(list);
-
-
+        System.out.println("List sorted by Insertion Sort:");
+        System.out.println(algs.insertionSort(list));
     }
 
     /**
@@ -34,10 +32,16 @@ public class Algorithms {
         List<Integer> listCopy = new ArrayList<>(list);
 
         for (int i = 0; i < listCopy.size() - 1; i++) {
+            boolean isSorted = true;
             for (int j = 0; j < listCopy.size() - i - 1; j++) {
                 if (listCopy.get(j) > listCopy.get(j + 1)) {
                     Collections.swap(listCopy, j, j + 1);
+                    isSorted = false;
                 }
+            }
+
+            if (isSorted) {
+                break;
             }
         }
 
@@ -45,8 +49,24 @@ public class Algorithms {
     }
 
     /**
-     * Insertion sort
+     * Insertion sort.
+     * Complexity: O(n^2).
+     * Better use for small arrays (number of elements < 100).
      */
+    public List<Integer> insertionSort(List<Integer> list) {
+        List<Integer> listCopy = new ArrayList<>(list);
+
+        for (int i = 1; i < listCopy.size(); i++) {
+            int j = i;
+
+            while (j > 0 && listCopy.get(j - 1) > listCopy.get(j)) {
+                Collections.swap(listCopy, j, j - 1);
+                j--;
+            }
+        }
+
+        return listCopy;
+    }
 
     /**
      * Selection sort
