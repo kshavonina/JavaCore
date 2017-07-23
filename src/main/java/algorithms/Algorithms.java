fa4d36f04a06c1eda20bd0119b2e2f11f55ad868
kshavonina <kshavonina@yandex.ru>
2017-07-23 20:11:07 +0300

    add bubble sort

--- src/main/java/algorithms/Algorithms.java
+++ src/main/java/algorithms/Algorithms.java
@@ -1,9 +1,70 @@
 package algorithms;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Algorithms {
     public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+        int listLength = (int) (Math.random() * 30 + 20);
+        System.out.println(listLength);
+
+        for (int i = 0; i < listLength; i++) {
+            list.add((int) (Math.random() * 200));
+        }
+
+        System.out.println(list);
+
+        Algorithms algs = new Algorithms();
+        System.out.println("List sorted by Bubble Sort:");
+        System.out.println(algs.bubbleSort(list));
+
+        System.out.println(list);
+
 
     }
 
+    /**
+     * Bubble sort.
+     * Complexity: O(n^2).
+     * Effective only for small arrays. Used for educational purposes.
+     */
+    public List<Integer> bubbleSort(List<Integer> list) {
+        List<Integer> listCopy = new ArrayList<>(list);
 
+        for (int i = 0; i < listCopy.size() - 1; i++) {
+            for (int j = 0; j < listCopy.size() - i - 1; j++) {
+                if (listCopy.get(j) > listCopy.get(j + 1)) {
+                    Collections.swap(listCopy, j, j + 1);
+                }
+            }
+        }
+
+        return listCopy;
+    }
+
+    /**
+     * Insertion sort
+     */
+
+    /**
+     * Selection sort
+     */
+
+    /**
+     * Merge sort
+     */
+
+    /**
+     * Quick sort
+     */
+
+    /**
+     * Radix sort
+     */
+
+    /**
+     * Binary search
+     */
 }
